c933cbf6098da33c9025fbd663007b3ba7830503
hongbo.mo <hongbo.mo@upai.com>
2014-12-22 20:18:12 +0800

    add try-except for debug

--- discuz.py
+++ discuz.py
@@ -3,7 +3,7 @@ import logging
 import logging.handlers
 import requests
 
-fmt_str = '[%(levelname)s %(asctime)s.%(msecs)d pid:%(process)d] %(message)s'
+fmt_str = '[%(levelname)s %(asctime)s.%(msecs)03d pid:%(process)d] %(message)s'
 date_str = '%y-%m-%d %H:%M:%S'
 handler = logging.handlers.RotatingFileHandler('discuz.log', maxBytes=10*1024*1024, backupCount=5)
 
@@ -27,23 +27,26 @@ class Robot:
         self.cookie = ''
 
     def sign_in(self):
-        session = requests.Session()
-        url = self.domain + '/member.php?mod=logging&action=login'
-        form = {'username': self.username, 'password': self.passwd, 'loginsubmit': 'true'}
-        p = session.post(url, form, timeout=1)
+        try:
+            session = requests.Session()
+            url = self.domain + '/member.php?mod=logging&action=login'
+            form = {'username': self.username, 'password': self.passwd, 'loginsubmit': 'true'}
+            p = session.post(url, form, timeout=2)
 
-        p = session.get(self.domain, timeout=1)
-        data = parseText(p)
-        res = re.match('.*name\=\"formhash\"\ value\=\"([^"]*)\".*', data, re.DOTALL)
-        hashval = res.group(1)
-        
-        url = self.domain + '/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1'
-        form = {'qdxq': 'kx', 'qdmode': '2', 'formhash': hashval, 'fastreply': '0'}
-        p = session.post(url, form, timeout=1)
-        data = parseText(p)
-        res = re.match('.*<div class="c">\r\n([^<]*).*', data, re.DOTALL)
-        msg = res.group(1)
-        discuzlog.info('user: %s %s' % (self.username, msg))
+            p = session.get(self.domain + '/forum.php', timeout=2)
+            data = parseText(p)
+            res = re.match('.*name\=\"formhash\"\ value\=\"([^"]*)\".*', data, re.DOTALL)
+            hashval = res.group(1)
+            
+            url = self.domain + '/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1'
+            form = {'qdxq': 'kx', 'qdmode': '2', 'formhash': hashval, 'fastreply': '0'}
+            p = session.post(url, form, timeout=2)
+            data = parseText(p)
+            res = re.match('.*<div class="c">\r\n([^<]*).*', data, re.DOTALL)
+            msg = res.group(1)
+            discuzlog.info('user: %s %s' % (self.username, msg))
+        except Exception as e:
+            discuzlog.error('user: %s %s %s' % (self.username, type(e).__name__, str(e)))
 
 
 Robot('http://bbs.zjut.edu.cn', 'mhb','8888891d3ea5ec27c39033b69120a18f').sign_in()
